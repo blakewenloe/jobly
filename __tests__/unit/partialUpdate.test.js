daf78c9b74aff21deb00fb7969ea39a7ad0c917f
Blake Enloe <blakewenloe@yahoo.com>
2020-11-13 02:29:01 -0600

    added sqlForPartialUpdate test

--- __tests__/unit/partialUpdate.test.js
+++ __tests__/unit/partialUpdate.test.js
@@ -1,9 +1,18 @@
-describe("partialUpdate()", () => {
-  it("should generate a proper partial update query with just 1 field",
-      function () {
+const sqlForPartialUpdate = require("../../helpers/partialUpdate");
 
-    // FIXME: write real tests!
-    expect(false).toEqual(true);
+describe("sqlForPartialUpdate()", () => {
+  it("should generate proper partial update query with 1 field", function () {
+    const { query, values } = sqlForPartialUpdate(
+      "users",
+      { first_name: "Test" },
+      "username",
+      "testuser"
+    );
 
+    expect(query).toEqual(
+      "UPDATE users SET first_name=$1 WHERE username=$2 RETURNING *"
+    );
+
+    expect(values).toEqual(["Test", "testuser"]);
   });
 });
