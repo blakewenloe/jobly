f9c8f42682a67b08d86b7580e29782ecc62905d1
Blake Enloe <blakewenloe@yahoo.com>
2020-11-13 17:38:28 -0600

    added jobs table

--- data.sql
+++ data.sql
@@ -14,3 +14,11 @@ CREATE TABLE companies (
     description text,
     logo_url text
 );
+CREATE TABLE jobs(
+    id SERIAL PRIMARY key,
+    title text NOT NULL,
+    salary float NOT NULL,
+    equity float CHECK (equity <= 1) NOT NULL,
+    company_handle text REFERENCES companies(handle) ON DELETE CASCADE,
+    date_posted TIMESTAMPTZ NOT NULL DEFAULT NOW()
+)
