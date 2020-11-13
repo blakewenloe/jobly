b7a729278ed5364532d57fb5a4cc5f6d321752d9
Blake Enloe <blakewenloe@yahoo.com>
2020-11-13 16:18:39 -0600

    added tests for post and patch requests

--- __tests__/unit/companies.test.js
+++ __tests__/unit/companies.test.js
@@ -11,3 +11,37 @@ describe("GET /companies", function () {
     expect(response.body.companies[0]).toHaveProperty("handle");
   });
 });
+
+describe("POST /companies", async function () {
+  beforeEach(async function () {
+    await db.query("DELETE FROM companies");
+  });
+  test("Creates a new company", async () => {
+    const response = await request(app).post(`/companies`).send({
+      handle: "sonys",
+      name: "Sonys",
+      num_employees: 500,
+      description: "Sony Entertainment",
+      logo_url: "https://www.sony.net/top/2017/img/icon/top-og.jpg",
+    });
+
+    expect(response.statusCode).toBe(201);
+    expect(response.body.company[0]).toHaveProperty("handle", "sonys");
+    expect(response.body.company[0]).toHaveProperty("num_employees", 500);
+  });
+});
+
+describe("PATCH /companies", async function () {
+  test("Updates a company", async () => {
+    const response = await request(app).patch(`/companies/sonys`).send({
+      handle: "sonys",
+      name: "Sonys",
+      num_employees: 122,
+      description: "Sony",
+      logo_url: "https://www.sony.net/top/2017/img/icon/top-og.jpg",
+    });
+    expect(response.statusCode).toBe(200);
+    expect(response.body.company[0]).toHaveProperty("description", "Sony");
+    expect(response.body.company[0]).toHaveProperty("num_employees", 122);
+  });
+});
